4d1b46b474dc5d138fac733d5f3dcdb5972df6b2
Andrew Kamarouski <andrew.kamarouski@outlook.com>
2016-06-15 21:24:15 +1000

    Add demo project

---
+++ demo/Messenger.Client.Demo/Controllers/FacebookController.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Messenger.Client.Objects;
+using Messenger.Client.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Messenger.Client.Demo.Controllers
+{
+    [Route("api/webhook")]
+    public class FacebookController : Controller
+    {
+        private readonly IMesengerMessageSender messageSender;
+
+        public FacebookController(IMesengerMessageSender messageSender)
+        {
+            this.messageSender = messageSender;
+        }
+
+        [HttpGet, Produces("text/html")]
+        public IActionResult Validate()
+        {
+            var challenge = Request.Query["hub.challenge"];
+
+
+            if (challenge.Any())
+            {
+                return Ok(challenge.First());
+            }
+
+            return Ok();
+        }
+
+        // Sends echo message back to sender.
+        [HttpPost]
+        public async Task HandleMessage([FromBody] MessengerObject obj)
+        {
+            foreach (var entry in obj.Entries)
+            {
+                await HandleEntry(entry);
+            }
+        }
+
+        private Task HandleEntry(MessengerEntry entry)
+        {
+            var messaging = entry.Messaging.First();
+            var text = messaging.Message.Text;
+            var response = new MessengerMessage {Text = text};
+            return messageSender.SendAsync(response, messaging.Sender);
+        }
+    }
+}
---
+++ demo/Messenger.Client.Demo/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Messenger.Client.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Messenger.Client.Demo
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddMessengerClient("%ACCESS_TOKEN%");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
